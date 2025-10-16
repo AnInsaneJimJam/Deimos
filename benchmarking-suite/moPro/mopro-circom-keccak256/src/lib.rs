51abab229f5422d71a33ab9a14fe62e08336fb3a
AnInsaneJimJam <anand47bansal@gmail.com>
2025-10-16 14:16:20 +0530

    Generated Plugin specific directories for multiple integration

---
+++ benchmarking-suite/moPro/mopro-circom-keccak256/src/lib.rs
@@ -0,0 +1,60 @@
+// Initializes the shared UniFFI scaffolding and defines the `MoproError` enum.
+mopro_ffi::app!();
+
+/// You can also customize the bindings by #[uniffi::export]
+/// Reference: https://mozilla.github.io/uniffi-rs/latest/proc_macro/index.html
+#[uniffi::export]
+fn mopro_uniffi_hello_world() -> String {
+    "Hello, World!".to_string()
+}
+
+#[macro_use]
+mod stubs;
+
+mod error;
+pub use error::MoproError;
+
+// CIRCOM_TEMPLATE
+// --- Circom Example of using groth16 proving and verifying circuits ---
+
+// Module containing the Circom circuit logic (Multiplier2)
+#[macro_use]
+mod circom;
+
+rust_witness::witness!(multiplier2);
+
+set_circom_circuits! {
+    ("multiplier2_final.zkey", circom_prover::witness::WitnessFn::RustWitness(multiplier2_witness)),
+}
+
+#[cfg(test)]
+mod circom_tests {
+    use crate::circom::{generate_circom_proof, verify_circom_proof, ProofLib};
+
+    const ZKEY_PATH: &str = "./test-vectors/circom/multiplier2_final.zkey";
+
+    #[test]
+    fn test_multiplier2() {
+        let circuit_inputs = "{\"a\": 2, \"b\": 3}".to_string();
+        let result =
+            generate_circom_proof(ZKEY_PATH.to_string(), circuit_inputs, ProofLib::Arkworks);
+        assert!(result.is_ok());
+        let proof = result.unwrap();
+        assert!(verify_circom_proof(ZKEY_PATH.to_string(), proof, ProofLib::Arkworks).is_ok());
+    }
+}
+
+
+// HALO2_TEMPLATE
+halo2_stub!();
+
+// NOIR_TEMPLATE
+noir_stub!();
+
+#[cfg(test)]
+mod uniffi_tests {
+    #[test]
+    fn test_mopro_uniffi_hello_world() {
+        assert_eq!(super::mopro_uniffi_hello_world(), "Hello, World!");
+    }
+}

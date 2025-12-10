aeec64115b258914b859e0d9a18c4f7e4b94c048
Mohit <mohit_g2@me.iitr.ac.in>
2025-12-11 01:32:09 +0530

    Added iOS app

--- benchmarking-suite/moPro/mopro-example-app/build.rs
+++ benchmarking-suite/moPro/mopro-example-app/build.rs
@@ -1,6 +1,7 @@
 fn main() {
-    // CIRCOM_TEMPLATE
-
-    rust_witness::transpile::transpile_wasm("./test-vectors/circom".to_string());
+    // ==============================================================================
+    // CIRCOM TEMPLATE (Uncomment to build for Circom)
+    // ==============================================================================
+    // rust_witness::transpile::transpile_wasm("./test-vectors/circom".to_string());
     
 }
